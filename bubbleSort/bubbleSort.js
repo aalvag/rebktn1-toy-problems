22971fd0613575f2be19a704914b7dbac4cd5e4a
ziedbarhoumi1989 <ziedbarhoumi1989@gmail.com>
2019-11-30 09:34:16 +0100

    solved

--- bubbleSort/bubbleSort.js
+++ bubbleSort/bubbleSort.js
@@ -35,4 +35,16 @@
 
 var bubbleSort = function(array) {
   // Your code here.
+  var j = 1
+  while(j <array.length-1){
+  for(var i = 0; i< array.length-j; i++) {
+  	if(array[i]>array[i+1]) {
+  		var tmp = array[i+1]
+  		array[i+1] = array[i]
+  		array[i]=tmp
+  	}
+  	j++
+  }
+  }
+  return array;
 };
