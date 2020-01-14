47018777e21fd071f50c5128008c1cb4924fce2a
ziedbarhoumi1989 <ziedbarhoumi1989@gmail.com>
2020-01-14 09:50:44 +0100

    'arrayception'

--- ArrayCeption/arrayCeption.js
+++ ArrayCeption/arrayCeption.js
@@ -15,3 +15,37 @@
 //  [ ] ==>	0
 //  array:
 //  [ [ [ ] ] ] ==>	0
+function Arrayception(array, depth = 0) {
+    var depth = 0
+    var maxdepth = 0
+    function checkArray(arr) {
+        for (var i = 0; i < arr.length; i++) {
+            if (!Array.isArray(arr[i]) && depth === 0) {
+                depth++;
+                if (depth > maxdepth) {
+                    maxdepth = depth
+                }
+            }
+
+            else return checkArray(arr[i], depth + 1)
+
+
+
+        }
+    }
+    checkArray(array)
+    // for (var i = 0; i < array.length; i++) {
+    //     if (!Array.isArray(array[i]) && depth === 0) {
+    //         depth++;
+    //         if (depth > maxdepth) {
+    //             maxdepth = depth
+    //         }
+    //     }
+
+    //     else Arrayception(array[i])
+
+
+
+    // }
+    return maxdepth;
+}
