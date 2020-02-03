2cca9862ae2e7b1102f71e3be37fe039a7b635c8
ziedbarhoumi1989 <ziedbarhoumi1989@gmail.com>
2020-02-03 09:42:06 +0100

    sum Of Primes

--- sumOfPrimes/sumOfPrime.js
+++ sumOfPrimes/sumOfPrime.js
@@ -4,5 +4,20 @@ Given a positive number, return the sum of all positive primes that are less tha
 */
 
 function sumOfPrimes(n) {
-    // your code here...
+    // your code here..
+  debugger;
+    var sumPrimes = 0
+    //find all primes that are less than n
+    for(var i = 2;i<n;i++) {
+    	var isprime = true;
+    	for(var j= 2;j<=i;j++) {
+    		if(i%j===0 && j!==i) {
+    			isprime=false
+    		}
+    	}
+    	if(isprime) {
+    		sumPrimes+=i
+    	}
+    }
+    return sumPrimes;
 }
